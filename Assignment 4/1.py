88eba4c64234f8cfc9d44c0ec1339eb7a0069b42
efreham1 <hammarberg83@gmail.com>
2023-12-07 10:25:16 +0100

    init
    Co-authored-by: Vincent Andersson <CoffeeWOSugar@users.noreply.github.com>

--- Assignment 4/1.py
+++ Assignment 4/1.py
@@ -2,3 +2,15 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
+K = 20
+Ts = 1/128
+
+
+k_array = np.linspace(0, K, endpoint = True)
+
+def x1(k):
+    np.cos(42*np.pi*k)
+
+def x2(k):
+    pass
+
